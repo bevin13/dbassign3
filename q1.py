efbbc7dfddd16bf43f88894bedcd774c874d05fe
bevin13 <64683144+bevin13@users.noreply.github.com>
2021-09-29 16:27:31 +0530

    Update q1.py

--- q1.py
+++ q1.py
@@ -3,20 +3,13 @@ import pandas as pd
 con = sqlite3.connect('database.sqlite')
 cur = con.cursor()
 
+print("Names of both the Home Teams and Away Teams in each match played in 2015 and FTHG = 5 : ")
 print(con.execute("Select HomeTeam,AwayTeam from Matches where Season ='2015' and FTHG ='5'").fetchall())
-print("**************************************************************************************************")
-
+print("Details of the matches where Arsenal is the Home Team and FTA is “A” : ")
 print(con.execute("Select * from Matches where HomeTeam = 'Arsenal' and FTR = 'A'").fetchall())
-print("**************************************************************************************************")
-
+print("Matches from the 2012 season till the 2015 season where Away Team is Bayern Munich and FTHG > 2 : ")
 print(con.execute("Select HomeTeam,AwayTeam,FTR from Matches where Season BETWEEN 2012 AND 2015 and AwayTeam = 'Bayern Munich' and FTHG > 2").fetchall())
-print("**************************************************************************************************")
-
+print("Matches where the Home Team name begins with “A” and Away Team name begins with “M” : ")
 print(con.execute("Select HomeTeam,AwayTeam,FTR from Matches where HomeTeam LIKE 'A%' and AwayTeam LIKE 'M%'").fetchall())
 
 con.close()
-
-
-
-
-
