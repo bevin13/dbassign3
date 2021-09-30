fc5eefa0768c995b24ae4e08c52cee6c23e0c22b
bevin13 <64683144+bevin13@users.noreply.github.com>
2021-09-30 12:45:01 +0530

    Add files via upload

---
+++ q3.py
@@ -0,0 +1,23 @@
+import sqlite3
+con = sqlite3.connect('database.sqlite')
+cur = con.cursor()
+
+print("HomeTeam, FTHG and FTAG from the 2010 season and  home team = Aachen in descending order.")
+print(con.execute("Select HomeTeam,FTHG,FTAG from Matches where Season = '2010' and HomeTeam = 'Aachen' ORDER BY FTHG DESC").fetchall())
+print("Total number of home games each team won during the 2016 season in descending order of number of home games.")
+print(con.execute("Select HomeTeam,COUNT(FTR) from Matches where FTR = 'H' and Season = '2016' GROUP BY HomeTeam ORDER BY COUNT(FTR) DESC").fetchall())
+print("Query that returns the first ten rows from the Unique_Teams table")
+print(con.execute("Select * from Unique_Teams LIMIT 10").fetchall())
+print("Match_ID and Unique_Team_ID with the corresponding Team_Name.")
+print("By using WHERE statement : ")
+print(con.execute("Select Teams_in_Matches.Match_id,Teams_in_Matches.Unique_Team_ID,Unique_Teams.TeamName from Teams_in_Matches,Unique_Teams where Teams_in_Matches.Unique_Team_ID = Unique_Teams.Unique_Team_ID LIMIT 10").fetchall())
+print("By using JOIN statement : ")
+print(con.execute("Select Teams_in_Matches.Match_id,Teams_in_Matches.Unique_Team_ID,Unique_Teams.TeamName from Teams_in_Matches JOIN Unique_Teams ON Teams_in_Matches.Unique_Team_ID = Unique_Teams.Unique_Team_ID LIMIT 10").fetchall())
+print("Query that joins together the Unique_Teams data table and the Teams table, returns the first 10 rows : ")
+print(con.execute("Select * from Unique_Teams JOIN Teams LIMIT 10").fetchall())
+print("Query that shows the Unique_Team_ID and TeamName from Unique_Teams and AvgAgeHome, Season and ForeignPlayersHome from Teams, return the first five rows : ")
+print(con.execute("Select Unique_Teams.Unique_Team_ID,Unique_Teams.TeamName,Teams.AvgAgeHome,Teams.Season,Teams.ForeignPlayersHome from Unique_Teams JOIN Teams ON Unique_Teams.TeamName = Teams.TeamName LIMIT 5").fetchall())
+print("Query that shows the highest Match_ID for each team that ends in a “y” or a “r”. Along with the maximum Match_ID, display the Unique_Team_ID Teams_in_Matches and the TeamName from Unique_Teams : ")
+print(con.execute("Select MAX(Match_id),Teams_in_Matches.Unique_Team_ID,Unique_Teams.TeamName from Teams_in_Matches JOIN Unique_Teams ON Teams_in_Matches.Unique_Team_ID = Unique_Teams.Unique_Team_ID where TeamName LIKE '%y' OR TeamName LIKE '%r' GROUP BY Teams_in_Matches.Unique_Team_ID, TeamName").fetchall())
+
+con.close()
